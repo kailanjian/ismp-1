a013366ce53f6490df95b00bd0532183e3615729
Iksoo Song <iksoo@stripe.com>
2020-06-18 22:04:59 -0700

    google tag manager

--- frontend/src/layout/Responsive/ResponsiveContainer.jsx
+++ frontend/src/layout/Responsive/ResponsiveContainer.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import DesktopContainer from './DesktopContainer';
 import MobileContainer from './MobileContainer';
 
-const ResponsiveContainer = ({ children }) => (
-  <div>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </div>
-);
+const ResponsiveContainer = ({ children }) => {
+  const history = useHistory();
+  history.listen(location => {
+//     ReactGA.set({ page: location.pathname }); // Update the user's current page
+//     ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': location.pathname,
+    'page_type': 'application'
+  });
+  console.log('window layer', window.dataLayer);
+
+  });
+  return (
+    <div>
+      <DesktopContainer>{children}</DesktopContainer>
+      <MobileContainer>{children}</MobileContainer>
+    </div>
+  );
+};
 
 export default ResponsiveContainer;
